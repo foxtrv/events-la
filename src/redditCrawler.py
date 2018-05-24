008e733433f163505bd050bd6b0c2fa28e7a0452
foxtrv <foxtrv@gmail.com>
2018-05-23 22:36:43 -0700

    gametime crawler complete

--- src/redditCrawler.py
+++ src/redditCrawler.py
@@ -1,9 +1,11 @@
 from colors import colors
 from bs4 import BeautifulSoup
 import requests
+import random
+randomNum = random.random()
 
 # First have to go to the subreddit and click on the new weekly post
-response = requests.get("https://www.reddit.com/r/LosAngeles/", headers = {'User-agent': 'event bot 1.0'})
+response = requests.get("https://www.reddit.com/r/LosAngeles/", headers = {'User-agent': 'event bot 1.0 .. random seed to thwart reddit API response rate' + str(randomNum)})
 soup = BeautifulSoup(response.text, "html.parser")
 a = soup.findAll(class_="thing")
 
@@ -13,7 +15,7 @@ for i in range(len(a)):
         if (title != None and title.find("Totally Awesome Things To Do In Los Angeles") > 0):
             link_suffix = a[i].find("a").get('href')
 
-# sometimes a returns NoneType, I am not sure why. My guess is that python is not fully waiting for the get to complete and uses the first thing it sees.
+# Sometimes this is null beacuse reddit has a API request rate limit.
 url = 'https://www.reddit.com' + link_suffix + '.json'
 
 # Now that you have the link, parse the json.
@@ -24,3 +26,4 @@ for i in page.json(): # parses 2 {} inside the outer [] -
             print (colors.red + "================ Collected from weekly post on /r/LosAngeles ================" + colors.black)
             print("Link = " + url)
             print (j['data']['body'])
+
